bf726b82235050ae44fae33fda98db97da239cc6
REMANTH <hulk102700@gmail.com>
2024-05-22 23:13:13 +0530

    2commit

---
+++ Controllers/userController.js
@@ -0,0 +1,86 @@
+import md5 from "md5";
+import pool from "../middlewares/db.js";
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const [results, fields] = await pool.execute(
+      "SELECT * FROM users WHERE username = ?",
+      [username]
+    );
+
+    if (results.length > 0) {
+      const user = results[0];
+      const hashedPassword = md5(password);
+
+      if (hashedPassword === user.password) {
+        console.log("Login successful");
+        res.status(200).json({
+          message: "Login successful",
+          user: {
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            pinno: user.pinno,
+            year: user.year,
+            semester: user.semester,
+            branch: user.branch,
+          },
+        });
+      } else {
+        console.log("Login failed: Incorrect password");
+        res.status(401).send("Invalid credentials");
+      }
+    } else {
+      console.log("Login failed: User not found");
+      res.status(401).send("Invalid credentials");
+    }
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+const register = async (req, res) => {
+  const { firstName, lastName, email, phoneNumber, userType, password } =
+    req.body;
+
+  // Input validation
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phoneNumber ||
+    !userType ||
+    !password
+  ) {
+    return res.status(400).send("Please provide all required fields");
+  }
+
+  try {
+    const [existingUsers, existingUsersFields] = await pool.execute(
+      "SELECT * FROM users WHERE email = ?",
+      [email]
+    );
+
+    if (existingUsers.length > 0) {
+      return res.status(400).send("Email is already taken");
+    }
+
+    const hashedPassword = md5(password);
+
+    const [results, fields] = await pool.execute(
+      "INSERT INTO users (firstName, lastName, email, phoneNumber, userType, password) VALUES (?, ?, ?, ?, ?, ?)",
+      [firstName, lastName, email, phoneNumber, userType, hashedPassword]
+    );
+
+    console.log("User registered successfully");
+    res.status(201).send("User registered successfully");
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+export default { login, register };
